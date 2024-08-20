7e81af066e73b5998be92b5784c776bdc28cc6bf
FanProgramer <90877394+FanProgramer@users.noreply.github.com>
2024-08-19 21:24:30 -0300

    Update get-comments.js

--- netlify/functions/get-comments.js
+++ netlify/functions/get-comments.js
@@ -1,27 +1,29 @@
-// get-comments.js
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./comments.db');
+// netlify/functions/get-comments.js
+const admin = require('firebase-admin');
+
+const serviceAccount = require('.casino-6de4f-firebase-adminsdk-4amut-8aa571de64.json64.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://casino-6de4f-default-rtdb.firebaseio.com'
+});
+
+
+const db = admin.database();
 
 exports.handler = async function(event) {
-    try {
-        const comments = await new Promise((resolve, reject) => {
-            db.all('SELECT userName, userComment FROM comments', [], (err, rows) => {
-                if (err) {
-                    return reject(err);
-                }
-                resolve(rows);
-            });
-        });
-
-        return {
-            statusCode: 200,
-            body: JSON.stringify(comments)
-        };
-    } catch (error) {
-        console.error('Error al obtener comentarios:', error);
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ error: 'Error al obtener comentarios.' })
-        };
-    }
+  try {
+    const snapshot = await db.ref('/comments').once('value');
+    const comments = snapshot.val();
+    
+    return {
+      statusCode: 200,
+      body: JSON.stringify(comments ? Object.values(comments) : [])
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: error.message })
+    };
+  }
 };
