d35feef565cf66f374c6f5777ce6e61e01415064
FanProgramer <90877394+FanProgramer@users.noreply.github.com>
2024-08-19 21:24:45 -0300

    Update save-comment.js

--- netlify/functions/save-comment.js
+++ netlify/functions/save-comment.js
@@ -1,47 +1,29 @@
-// save-comment.js
-const sqlite3 = require('sqlite3').verbose();
-const path = require('path');
+// netlify/functions/save-comment.js
+const admin = require('firebase-admin');
+
+const serviceAccount = require('.casino-6de4f-firebase-adminsdk-4amut-8aa571de64.json64.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://casino-6de4f-default-rtdb.firebaseio.com'
+});
+
+const db = admin.database();
 
 exports.handler = async function(event) {
-    if (event.httpMethod === 'POST') {
-        try {
-            const data = JSON.parse(event.body);
-            const { userComment, userName } = data;
+  const { userComment, userName } = JSON.parse(event.body);
 
-            if (!userComment || !userName) {
-                return {
-                    statusCode: 400,
-                    body: JSON.stringify({ message: 'Faltan datos' })
-                };
-            }
-
-            const dbPath = path.join(__dirname, 'comments.db');
-            const db = new sqlite3.Database(dbPath);
-
-            db.serialize(() => {
-                db.run("CREATE TABLE IF NOT EXISTS comments (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, comment TEXT)");
-                const stmt = db.prepare("INSERT INTO comments (name, comment) VALUES (?, ?)");
-                stmt.run(userName, userComment);
-                stmt.finalize();
-            });
-
-            db.close();
-
-            return {
-                statusCode: 200,
-                body: JSON.stringify({ message: 'Comentario guardado exitosamente' })
-            };
-        } catch (error) {
-            console.error('Error al guardar comentario:', error);
-            return {
-                statusCode: 500,
-                body: JSON.stringify({ message: 'Error al guardar comentario' })
-            };
-        }
-    } else {
-        return {
-            statusCode: 405,
-            body: JSON.stringify({ message: 'Método no permitido' })
-        };
-    }
+  try {
+    await db.ref('/comments').push({ userComment, userName });
+    
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Comment saved successfully' })
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: error.message })
+    };
+  }
 };
