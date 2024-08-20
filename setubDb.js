345709ed7a2c5b826bc6f9a60351c04a82aaf47d
Fabian Seleme <Fabian.Seleme@proveedor.toyota.com.ar>
2024-08-20 18:31:00 -0300

    base de datos

--- setubDb.js
+++ setubDb.js
@@ -1,6 +1,6 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
-const dbPath = path.resolve(__dirname, 'commentis.db');
+const dbPath = path.resolve(__dirname, 'data.db');
 
 const db = new sqlite3.Database(dbPath);
 
