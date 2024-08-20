440419bee794f3961111f2a65c50426598c7c9e0
Fabian Seleme <Fabian.Seleme@proveedor.toyota.com.ar>
2024-08-20 18:58:11 -0300

    resolviendo base

--- setubDb.js
+++ setubDb.js
@@ -1,11 +1,22 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
-const dbPath = path.resolve(__dirname, 'data.db');
+const dbPath = path.resolve(__dirname, '../data.db');
 
-const db = new sqlite3.Database(dbPath);
+exports.addComment = (userName, comment) => {
+  const db = new sqlite3.Database(dbPath);
 
-db.serialize(() => {
-  db.run('CREATE TABLE IF NOT EXISTS comments (id INTEGER PRIMARY KEY AUTOINCREMENT, comment TEXT)');
-});
-
-db.close();
+  return new Promise((resolve, reject) => {
+    db.run(
+      'INSERT INTO comments (user_name, comment) VALUES (?, ?)',
+      [userName, comment],
+      function(err) {
+        if (err) {
+          reject({ statusCode: 500, body: 'Error inserting comment' });
+        } else {
+          db.close();
+          resolve({ statusCode: 200, body: 'Comment added successfully' });
+        }
+      }
+    );
+  });
+};
